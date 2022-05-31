3519f69f39e216a0c90fbc2a159c261aea87d487
goldenacid12 <alan.emilio.admj.li@gmail.com>
2022-05-31 15:35:47 +0700

    crop berhasil untuk gallery saja

--- Frood App/FroodApp/app/src/main/java/com/frood/app/presentation/ui/scan/ScanFragment.kt
+++ Frood App/FroodApp/app/src/main/java/com/frood/app/presentation/ui/scan/ScanFragment.kt
@@ -1,29 +1,64 @@
 package com.frood.app.presentation.ui.scan
 
+import android.Manifest.permission.CAMERA
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.findNavController
-import com.frood.app.R
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.frood.app.databinding.FragmentScanBinding
+import com.theartofdev.edmodo.cropper.CropImage
+import java.io.File
 
 class ScanFragment : Fragment() {
 
-    private lateinit var binding: FragmentScanBinding
+    private var _binding: FragmentScanBinding? = null
+    private val binding get() = _binding!!
+    private var getFile: File? = null
+    private lateinit var cropActivityResultLauncher: ActivityResultLauncher<Any?>
+    private val cropActivityResultContract = object : ActivityResultContract<Any?, Uri?>(){
+        override fun createIntent(context: Context, input: Any?): Intent {
+            return CropImage.activity().setAspectRatio(1,1).getIntent(context)
+        }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_scan, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
-
-        binding.imageClose.setOnClickListener { findNavController().navigateUp() }
-        return binding.root
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+            return CropImage.getActivityResult(intent)?.uri
+        }
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
+        cropActivityResultLauncher = registerForActivityResult(cropActivityResultContract){
+            it?.let { uri ->
+                val myFile = uriToFile(uri, requireContext())
+                getFile = myFile
+                binding.previewImageView.setImageURI(uri)
+            }
+        }
+        binding.selectImage.setOnClickListener{cropActivityResultLauncher.launch(null)}
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
