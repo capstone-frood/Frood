1ebb29c322da808e19b4c57663fb8e618cad42d9
akaslayer <hendrysurijanto123@gmail.com>
2022-06-02 10:38:29 +0700

    menambahkan model

--- main.py
+++ main.py
@@ -60,6 +60,8 @@
 
 
 from flask import Flask, request, jsonify
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 from io import BytesIO
@@ -68,6 +70,8 @@ import os
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = {'jpg','jpeg'}
+
+model = load_model(Model_10_Class_Acc99.h5)
 
 
 def allowed_file(filename):
@@ -94,12 +98,48 @@ def predict():
     if image and allowed_file(image.filename):
         image = image.read()
         img = resize(image)
-        result = predicted(img)
-        if result == 1:
-            Status = "Fresh"
+        result = model.predict(img)
+        if result == 0:
+            Status = "Fresh Apple"
             expired = "7 Days (Refrigirator temps)"
             Note = "Cook it before rotten"
-
+        elif result ==1:
+            Status = "Fresh Banana"
+            expired = "2 Days (Refrigirator temps)"
+            Note = "Cook it before rotten"
+        elif result ==2:
+            Status = "Fresh Orange"
+            expired = "8 Days (Refrigirator temps)"
+            Note = "Eat it quickly"
+        elif result ==3:
+            Status = "Fresh Strawberry"
+            expired = "21 Days (Refrigirator temps)"
+            Note = "Eat it quickly"
+        elif result ==4:
+            Status = "Fresh Tomato"
+            expired = "12 Days (Refrigirator temps)"
+            Note = "Eat it quickly"
+        elif result ==5:
+            Status = "Spoil Apple"
+            expired = "1 Days (Refrigirator temps)"
+            Note = "Eat it quickly"
+        elif result ==6:
+            Status = "Spoil Banana"
+            expired = "1 Days (Refrigirator temps)"
+            Note = "Don't eat if you want to be healthy"
+        elif result ==7:
+            Status = "Spoil Orange"
+            expired = "2 Days (Refrigirator temps)"
+            Note = "Eat it quickly"
+        elif result ==8:
+            Status = "Spoil Strawberry"
+            expired = "2 Days (Refrigirator temps)"
+            Note = "Eat it quickly"
+        elif result ==9:
+            Status = "Spoil Tomato"
+            expired = "12 Days (Refrigirator temps)"
+            Note = "Eat it quickly"
+            
         return jsonify({'Status' : Status,'Expired':expired,'Note':Note})
     else:
         resp = jsonify({'message': 'Image extension is not allowed'})
