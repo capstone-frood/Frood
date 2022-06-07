8428c39466471e85529369844aa3f71ad6a5b78e
akaslayer <hendrysurijanto123@gmail.com>
2022-06-07 21:04:20 +0700

    Update main.py

--- CC/main.py
+++ CC/main.py
@@ -36,11 +36,12 @@ def predict():
         img = resize(image)
         results = model.predict(img)
         max_value = np.max(results)
-        if max_value<=0.08:
+        print(max_value)
+        if max_value<=0.8:
             resp = jsonify({'message': 'Image can not be predicted'})
             resp.status_code = 400
             return resp
-        elif max_value>0.08:
+        elif max_value>0.8:
             result = np.argmax(results, axis=1)
             if result == 0:
                 Status = "Fresh Apple"+" ({:.0%})".format(max_value)
@@ -55,46 +56,62 @@ def predict():
                 expired = "5-6 Days (Refrigirator temps)"
                 Note = "Buy it immedietly"
             elif result ==3:
+                Status = "Fresh Kangkung"+" ({:.0%})".format(max_value)
+                expired = "3 Days (Refrigirator temps)"
+                Note = "Buy it immedietly"
+            elif result ==4:
                 Status = "Fresh Orange"+" ({:.0%})".format(max_value)
                 expired = "30 Days (Refrigirator temps)"
                 Note = "Eat it slowly, you still have a lot of time"
-            elif result ==4:
+            elif result ==5:
+                Status = "Fresh Sawi"+" ({:.0%})".format(max_value)
+                expired = "2-5 Days (Refrigirator temps)"
+                Note = "Buy it immedietly"
+            elif result ==6:
                 Status = "Fresh Spinach"+" ({:.0%})".format(max_value)
                 expired = "1 Days (Refrigirator temps)"
                 Note = "Cook it immediately after you buy it"
-            elif result ==5:
+            elif result ==7:
                 Status = "Fresh Strawberry"+" ({:.0%})".format(max_value)
                 expired = "10 Days (Refrigirator temps)"
                 Note = "Eat it quickly"
-            elif result ==6:
+            elif result ==8:
                 Status = "Fresh Tomato"+" ({:.0%})".format(max_value)
                 expired = "20 Days (Refrigirator temps)"
                 Note = "Eat it slowly, you still have a lot of time"
-            elif result ==7:
+            elif result ==9:
                 Status = "Spoil Apple"+" ({:.0%})".format(max_value)
                 expired = "Already Expired"
                 Note = "Throw it away"
-            elif result ==8:
+            elif result ==10:
                 Status = "Spoil Banana"+" ({:.0%})".format(max_value)
                 expired = "Already Expired"
                 Note = "Don't ever eat that"
-            elif result ==9:
+            elif result ==11:
                 Status = "Spoil Chili"+" ({:.0%})".format(max_value)
                 expired = "Already Expired"
                 Note = "Throw it away immediately in the trashcan"
-            elif result ==10:
+            elif result ==12:
+                Status = "Spoil Kangkung"+" ({:.0%})".format(max_value)
+                expired = "Already Expired"
+                Note = "Please don't cook and eat"
+            elif result ==13:
                 Status = "Spoil Orange"+" ({:.0%})".format(max_value)
                 expired = "Already Expired"
                 Note = "Remember to never eat that"
-            elif result ==11:
+            elif result ==14:
+                Status = "Spoil Sawi"+" ({:.0%})".format(max_value)
+                expired = "Already Expired"
+                Note = "Please don't cook and eat"
+            elif result ==15:
                 Status = "Spoil Spinach"+" ({:.0%})".format(max_value)
                 expired = "Already Expired"
                 Note = "The spinach is no longer fit for consumption"
-            elif result ==12:
+            elif result ==16:
                 Status = "Spoil Strawberry"+" ({:.0%})".format(max_value)
                 expired = "Already Expired"
                 Note = "Don't ever think about eating it"
-            elif result ==13:
+            elif result ==17:
                 Status = "Spoil Tomato"+" ({:.0%})".format(max_value)
                 expired = "Already Expired"
                 Note = "Throw it away"
